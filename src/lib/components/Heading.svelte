8f9af2558c137f15f6399549e1bd551f10fc8c4d
ndri <5224431+ndri@users.noreply.github.com>
2025-09-06 17:22:27 +0200

    Add dark mode

--- src/lib/components/Heading.svelte
+++ src/lib/components/Heading.svelte
@@ -7,14 +7,16 @@
 	}
 
 	const { level, children }: Props = $props();
+
+	const colourClasses = 'text-accent-800 dark:text-accent-300';
 </script>
 
 {#if level === 1}
-	<h1 class="text-2xl font-bold text-accent-800">
+	<h1 class={['text-2xl font-bold', colourClasses]}>
 		{@render children()}
 	</h1>
 {:else if level === 2}
-	<h2 class="text-lg font-medium text-accent-800">
+	<h2 class={['text-lg font-medium', colourClasses]}>
 		{@render children()}
 	</h2>
 {/if}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,7 +9,13 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<div class="flex min-h-full justify-center bg-main-100 px-8 py-16 text-main-800">
+<div
+	class={[
+		'flex min-h-full justify-center px-8 py-16',
+		'bg-main-100 text-main-800',
+		'dark:bg-main-800 dark:text-main-100'
+	]}
+>
 	<main class="flex w-full max-w-3xl flex-col gap-4">
 		{@render children?.()}
 	</main>
