ea75a19e8b99439dd77fa355c58983489d632886
ndri <5224431+ndri@users.noreply.github.com>
2025-10-04 22:43:39 +0200

    Improve confidence selector

--- src/lib/components/ConfidenceSelector.svelte
+++ src/lib/components/ConfidenceSelector.svelte
@@ -17,56 +17,15 @@
 	}
 
 	let { selectedConfidence = $bindable(), disabled, highlightSelected }: Props = $props();
-
-	let selectedValue = $state<keyof typeof CONFIDENCES | undefined>(
-		getKeyByValue(selectedConfidence) as keyof typeof CONFIDENCES
-	);
-
-	function getKeyByValue(value: number | undefined) {
-		if (value === undefined) return undefined;
-
-		const entry = Object.entries(CONFIDENCES).find(([, value]) => value === selectedConfidence);
-		if (entry) {
-			return entry[0];
-		} else {
-			return undefined;
-		}
-	}
-
-	$effect(() => {
-		if (selectedValue) {
-			selectedConfidence = CONFIDENCES[selectedValue];
-		} else {
-			selectedConfidence = undefined;
-		}
-	});
-
-	explicitEffect(
-		() => {
-			if (selectedValue) {
-				selectedConfidence = CONFIDENCES[selectedValue];
-			} else {
-				selectedConfidence = undefined;
-			}
-		},
-		() => [selectedValue]
-	);
-
-	explicitEffect(
-		() => {
-			if (selectedConfidence) {
-				selectedValue = getKeyByValue(selectedConfidence) as keyof typeof CONFIDENCES;
-			} else {
-				selectedValue = undefined;
-			}
-		},
-		() => [selectedConfidence]
-	);
 </script>
 
 <ButtonGroup
-	buttons={Object.keys(CONFIDENCES).map((value) => ({ value, shortcutKey: String(value)[0] }))}
-	bind:selectedValue
+	buttons={Object.entries(CONFIDENCES).map(([label, value]) => ({
+		value,
+		label,
+		shortcutKey: label[0]
+	}))}
+	bind:selectedValue={selectedConfidence}
 	{disabled}
-	highlightedValue={highlightSelected ? getKeyByValue(selectedConfidence) : undefined}
+	highlightedValue={highlightSelected ? selectedConfidence : undefined}
 />
